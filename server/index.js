04d7d07b1026edb6540990b026c978764cea3378
scsigma <sully@sigmacomputing.com>
2023-10-11 17:03:03 -0700

    added hightouch sync capability

--- server/index.js
+++ server/index.js
@@ -1,18 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch'); 
+
 
 const port = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get('/test', (req, res) => {
     res.send({ data: "Hello from the server!"})
 })
 
-app.post('/hightouch_sync', (req, res) => {
-    const {token, syncId} = req.body;
-    res.json({ token, syncId})
-    // fetch('https://api.hightouch.com/api/v1/syncs/1644239/trigger')
+app.post('/hightouch_sync', async (req, res) => {
+    const {apiTokenoken, syncId} = req.body;
+    // res.json({ token, syncId})
+    await fetch(`https://api.hightouch.com/api/v1/syncs/${syncId}/trigger`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiToken}`
+        }
+    })
+
+    res.json({ apiToken, syncId})
 })
 
 app.listen(port, () => {
